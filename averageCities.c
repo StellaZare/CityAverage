9d3982190df9ad85330e36c1baed228b20e085ea
Stella Zarei <zareistella@gmail.com>
2023-03-06 19:38:13 -0800

    Starter code

---
+++ averageCities.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+/* ---------- Constants and global variables ---------- */
+
+#define TRUE 1
+#define FALSE 0
+
+/* ---------- Helper functions ---------- */
+
+/* ---------- Main function ---------- */
+int main(int argc, char* argv[]){
+
+
+    return 0;
+}
