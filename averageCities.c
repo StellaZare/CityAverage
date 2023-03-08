f5caa637bcdd34e04f403564df5b2559d66c08d0
Stella Zarei <zareistella@gmail.com>
2023-03-08 12:09:35 -0800

    adds printing of overall data

--- averageCities.c
+++ averageCities.c
@@ -13,19 +13,36 @@
 #define MAX_LENGTH 100
 
 pthread_mutex_t mutex;
+float max_temp_overall;
+char max_temp_location[100];
+float min_temp_overall;
+char min_temp_location[100];
+float sum_overall;
+int num_entries_overall;
 
 char cities[MAX_CITIES][MAX_LENGTH] = {"Charlottetown.dat", "Edmonton.dat", "Halifax.dat", "Montreal.dat",
     "Ottawa.dat", "Quebec.dat", "Toronto.dat", "Vancouver.dat", "Victoria.dat", "Winnipeg.dat"};
 
 /* ---------- Helper functions ---------- */
-void printData(char filename[], float min_temp, float max_temp, float avg, int num_entries){
+void printData(char filename[], float min_temp, float max_temp, float sum_temp, int num_entries){
     pthread_mutex_lock(&mutex);
+    if (max_temp > max_temp_overall){
+        max_temp_overall = max_temp;
+        strcpy(max_temp_location, filename);
+    }
+    if(min_temp < min_temp_overall){
+        min_temp_overall = min_temp;
+        strcpy(min_temp_location, filename);
+    }
+    sum_overall += sum_temp;
+    num_entries_overall += num_entries;
+
     printf("===========================================\n");
     printf("File                %s\n", filename);
     printf("Number of entries   %d\n", num_entries);
     printf("Min temprature      %.2f degrees\n", min_temp);
     printf("Max temprature      %.2f degrees\n", max_temp);
-    printf("Average temprature  %.2f degrees\n", avg);
+    printf("Average temprature  %.2f degrees\n", sum_temp/num_entries);
     pthread_mutex_unlock(&mutex);
 }
 
@@ -39,20 +56,18 @@ void* processCity(char filename[]){
     char line[MAX_LENGTH];
     float min_temp = 100;
     float max_temp = -100;
-    float avg = 0;
+    float sum_temp = 0;
     int num_entries = -1;
 
     while(fgets(line, MAX_LENGTH, file)){
         sscanf(line, "%f %f", &data[0], &data[1]);
         max_temp = (data[0] > max_temp) ? data[0]: max_temp;
         min_temp = (data[1] < min_temp) ? data[1]: min_temp;
-        avg = avg + data[0] + data[1];
+        sum_temp = sum_temp + data[0] + data[1];
         num_entries++;
     }
     fclose(file);
-
-    avg = avg/num_entries;
-    printData(filename, min_temp, max_temp, avg, num_entries);
+    printData(filename, min_temp, max_temp, sum_temp, num_entries);
     return NULL;
 }
 
@@ -95,6 +110,11 @@ int main(int argc, char* argv[]){
     pthread_mutex_destroy(&mutex);
     end_time = clock();
     printf("===========================================\n");
-    printf("Clock cycles    %ld\n", end_time - start_time);
+    printf("Number of files     %d\n", MAX_CITIES);
+    printf("Number of entries   %d\n", num_entries_overall);
+    printf("Min temprature     %.2f degrees in %s\n", min_temp_overall, min_temp_location);
+    printf("Max temprature      %.2f degrees in %s\n", max_temp_overall, max_temp_location);
+    printf("Average temprature  %.2f degrees\n", sum_overall/num_entries_overall);
+    printf("Clock cycles        %ld\n", end_time - start_time);
     return 0;
 }
